054b70d2be9be302383aa78fe3a7c7efe8570fec
goranradmanovic <goranradmanovic@gmail.com>
2019-04-30 22:29:35 +0200

    Adding name attribute to the contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,7 +5,7 @@
       form(action='https://formspree.io/goranradmanovic@gmail.com' method='post' autocomplete='off' class='contact__form')
         input(type='text' name='name' class='contact__form--name' maxlength='50' minlength='5' pattern='^([- \\w\\d\\u00c0-\\u024f]+)$' placeholder='Име и Презиме' id='name' title='Ваше пуно име (специјални карактери нису дозвољени)' required)
         input(type='email' name='_replyto' class='contact__form--email' maxlength='50' pattern='^(([-\\w\\d]+)(\\.[-\\w\\d]+)*@([-\\w\\d]+)(\\.[-\\w\\d]+)*(\\.([a-zA-Z]{2,5}|[\\d]{1,3})){1,2})$' placeholder='Емаил' id='email' title='Валидна емаил адреса' required)
-        textarea(class='contact__form--message' maxlength='1500' placeholder='Порука' id='message' required)
+        textarea(name='message' class='contact__form--message' maxlength='1500' placeholder='Порука' id='message' required)
         input(type='submit' value='Пошаљи' class='contact__form--btn')
 
       iframe(id='gmap_canvas' class='contact__map' src='https://maps.google.com/maps?q=Mitropolita%20Georgija%20Nikolajevi%C4%87a%2050%2C%20Gradi%C5%A1ka&t=&z=15&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0')
