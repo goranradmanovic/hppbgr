b35ede4dab574ef880088b18123ce2f414662524
goranradmanovic <goranradmanovic@gmail.com>
2019-05-02 22:14:14 +0200

    Commenting vue js config option

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hppbgr/' : '/',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/hppbgr/' : '/',
 
   css: {
     loaderOptions: {
