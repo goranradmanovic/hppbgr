07e382610dcdc633ebdb9adea275a8893c3f53a3
goranradmanovic <goranradmanovic@gmail.com>
2019-05-03 21:51:46 +0200

    Removing footer section and adding to single component

--- src/App.vue
+++ src/App.vue
@@ -10,47 +10,18 @@
       Aside
 
     footer(class='grid__footer')
-      .grid__footer--container
-        .grid__footer--container--logo
-
-        .grid__footer--container--nav
-          ul(class='grid__footer--container--nav--list')
-            li(v-for='allPageLink in allPageLinks' class='grid__footer--container--nav--list--item')
-              router-link(:key='allPageLink.name' :to='allPageLink.path' class='grid__footer--container--nav--list--item--link') {{ allPageLink.name }}
-
-        .grid__footer--container--info
-          address(class='grid__footer--container--info--address')
-            .grid__footer--container--info--address--item Храм Покорова Пресвете Богородице
-            .grid__footer--container--info--address--item Митрополита Георгија Николајевића,
-            .grid__footer--container--info--address--item 78400 Градишка, Република Српска
-            .grid__footer--container--info--address--item Тел: ++387 051 000 000
-            .grid__footer--container--info--address--item Емаил:&nbsp;
-              a(href='mailto: example@example.com' class='grid__footer--container--info--address--item--email') example@example.com
-            .grid__footer--container--info--address--item
-              router-link(:to="{ name: 'home', params: {} }" target='_blank' class='grid__footer--container--info--address--item--link') Храм Покорова Пресвете Богородице
-            .grid__footer--container--info--address--item &copy; Copyright {{ getCurrentYear }}
-            .grid__footer--container--info--address--item  Дизајн и Развој
-              a(href='https://goranradmanovic.github.io/' target='_blank' class='grid__footer--container--info--address--item--link')  ГР
+      Footer
 </template>
 
 <script>
   import Navigation from '@/components/Navigation.vue';
   import Aside from '@/components/Aside.vue';
+  import Footer from '@/components/Footer.vue';
 
   export default {
     name: 'app',
     components: {
-      Navigation, Aside
-    },
-
-    computed: {
-      allPageLinks() {
-        return this.$store.state.navLinks;
-      },
-
-      getCurrentYear() {
-        return this.$store.state.currentYear;
-      }
+      Navigation, Aside, Footer
     }
   }
 </script>
